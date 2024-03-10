36595c9ee348d17250804b4b922d9d2ce8c7b15d
brady.ouren <tippenein@gmail.com>
2024-03-10 13:09:15 -0700

    initial plotting

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, redirect, request, url_for, render_template
+from flask import Flask, redirect, request, url_for, render_template, send_file
 from flask_socketio import SocketIO
 import sqlite3
 import os
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+import src.plot as plot
+import io
+import base64
 
 app = Flask(__name__)
 
@@ -17,6 +23,23 @@ def get_db_connection():
 @app.route('/')
 def home():
     return redirect(url_for('get_tokens'))
+
+# @app.route('/all')
+# def top():
+#     # Fetch and process data from the local database
+#     data = plot.plot_ohlc_data()
+
+
+#     # Generate a plot
+#     img = io.BytesIO()
+#     plt.figure()
+#     data.plot(kind='bar')
+#     plt.savefig(img, format='png')
+#     img.seek(0)
+#     plot_url = base64.b64encode(img.getvalue()).decode()
+
+#     # Render the template with the plot
+#     return render_template('plot.html', plot_url=plot_url)
 
 @app.route('/tokens')
 def get_tokens():
@@ -44,6 +67,23 @@ def get_tokens():
 
     return render_template('list_view.html', tokens=tokens, sort_by=sort_by, sort_order=sort_order)
 
+@app.route('/tokens/<token>')
+def token_detail(token):
+    date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    bot_timestamp = int(plot.datetime_to_epoch(date) - 18000*2)
+    pool_address = plot.get_solana_pool_address(token)
+    image = plot.plot_ohlc_data(pool_address, bot_timestamp)
+    if image is not None:
+        # Encode the image bytes to base64
+        image_base64 = base64.b64encode(image).decode('utf-8')
+        return render_template('plot.html', token=token, image=image_base64)
+    else:
+        print("shit")
+        return "Could not generate plot", 404
+
+
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     socketio.run(app, host='0.0.0.0', port=port, debug=DEBUG)
