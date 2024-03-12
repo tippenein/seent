2c3b2092cb870de03c45bd0ed8e7d1665c447348
brady.ouren <tippenein@gmail.com>
2024-03-12 16:56:22 -0700

    python, no rel paths

--- src/app.py
+++ src/app.py
@@ -5,9 +5,9 @@ import os
 import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
-from .plot import datetime_to_epoch, get_solana_pool_address, plot_ohlc_data
+from plot import datetime_to_epoch, get_solana_pool_address, plot_ohlc_data
 import io
-from .db import DATABASE_CONFIG, DATABASE_TYPE, DatabaseController
+from db import DATABASE_CONFIG, DATABASE_TYPE, DatabaseController
 import base64
 
 
